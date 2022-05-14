043a49ba8aae2fbcb3353ae63ff952606c0b22e0
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-05-14 15:37:40 +0530

    Time: 25 ms (26.52%), Space: 46.4 MB (43.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,7 +4,18 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         // return recursive(text1,text2,m,n);
-        return dpMemoize(text1,text2,m,n,new int[m+1][n+1]);
+        // return dpMemoize(text1,text2,m,n,new int[m+1][n+1]);
+        int[][] dp = new int[m+1][n+1];
+    for(int i = 1;i<m+1;i++){
+        for(int j = 1;j<n+1;j++){
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }else{
+                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[m][n];
 }
 //     private int recursive(String s, String t, int m, int n) {
         
@@ -14,16 +25,17 @@ class Solution {
 //             return Math.max(recursive(s,t,m-1,n),recursive(s,t,m,n-1));
 //         }
 //     }
-    private int dpMemoize(String s,String t,int m,int n, int[][] dp){
-        if (m == 0 || n == 0) return 0;
-        if(dp[m][n]!=0){
-            return dp[m][n];
-        }
-        if (s.charAt(m - 1) == t.charAt(n - 1)) {
-            dp[m][n] =  1 + dpMemoize(s, t, m - 1, n - 1,dp);
-        } else{
-            dp[m][n] =  Math.max(dpMemoize(s,t,m-1,n,dp),dpMemoize(s,t,m,n-1,dp));
-        }
-        return dp[m][n];
-    }
+    // private int dpMemoize(String s,String t,int m,int n, int[][] dp){
+    //     if (m == 0 || n == 0) return 0;
+    //     if(dp[m][n]!=0){
+    //         return dp[m][n];
+    //     }
+    //     if (s.charAt(m - 1) == t.charAt(n - 1)) {
+    //         dp[m][n] =  1 + dpMemoize(s, t, m - 1, n - 1,dp);
+    //     } else{
+    //         dp[m][n] =  Math.max(dpMemoize(s,t,m-1,n,dp),dpMemoize(s,t,m,n-1,dp));
+    //     }
+    //     return dp[m][n];
+    // }
+    
 }
