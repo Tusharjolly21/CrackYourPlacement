cddd4be6ad38281fe26eb4d2c86184ee9d31cc78
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-07-06 14:16:59 +0530

    Time: 239 ms (6.59%), Space: 58 MB (13.57%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,30 +1,25 @@
 class Solution {
+
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i < amount + 1; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = i / coins[0];
+            else
+                dp[0][i] = (int) Math.pow(10, 9);
         }
-        int ans =  totalCoins(coins,n-1,amount,dp);
-        if(ans >= (int)Math.pow(10,9)) return -1;
-        else return ans;
-    }
-    private int totalCoins(int[] coins,int n,int amount,int[][] dp){
-        if(n==0){
-            if(amount%coins[0]==0){
-                return amount/coins[0];
-            }else{
-                return (int)Math.pow(10,9);
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                int nonPick = 0 + dp[i - 1][j];
+                int pick = (int) Math.pow(10, 9);
+                if (coins[i-1] <= j) {
+                    pick = 1 + dp[i][j - coins[i-1]];
+                }
+                dp[i][j] = Math.min(pick, nonPick);
             }
         }
-        if(dp[n][amount]!=-1) return dp[n][amount];
-        int notTaken = 0 + totalCoins(coins,n-1,amount,dp);
-    
-    int taken = (int)Math.pow(10,9);
-    if(coins[n] <= amount)
-        taken = 1 + totalCoins(coins,n,amount-coins[n],dp);
-        
-    dp[n][amount] =  Math.min(notTaken,taken);
-        return dp[n][amount];
+         if(dp[n][amount]>=(int)Math.pow(10,9)) return -1;
+        else return dp[n][amount];
     }
 }
