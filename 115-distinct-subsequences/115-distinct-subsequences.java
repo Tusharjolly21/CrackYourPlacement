92ae4ba1dc6287ea4ce081cf5d53b83d65df851b
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-07-07 22:55:38 +0530

    Time: 43 ms (16.48%), Space: 50.4 MB (38.08%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,31 +1,25 @@
 class Solution {
-    public int numDistinct(String s, String t) {
+ public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n][m];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 1;
         }
-        return numDis(s,t,n-1,m-1,dp);
-    }
-    private int numDis(String s,String t,int n,int m,int[][] dp){
-        if(m<0){
-            return 1;
+        for (int i = 1; i < m + 1; i++) {
+            dp[0][i] = 0;
         }
-        if(n<0){
-            return 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1))
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
         }
-        if(dp[n][m]!=-1) return dp[n][m];
-        if(s.charAt(n)==t.charAt(m)){
-            int leaveOne = numDis(s,t,n-1,m-1,dp);
-            int stay = numDis(s,t,n-1,m,dp);
-            dp[n][m] =  (leaveOne+stay);
-        }else{
-            dp[n][m] =  numDis(s,t,n-1,m,dp);
-            
-        }
+
         return dp[n][m];
-            
     }
-    
 }
