503c219918dbb8bfc6d68a524e08ffcd74773ebd
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-07-15 15:50:28 +0530

    Time: 2 ms (92.90%), Space: 42.6 MB (28.64%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];        
+        for(char c:s.toCharArray()) ++freq[c];
+        int OddGroup = 0;
+        for(int i:freq) OddGroup += i & 1;
+        return s.length() - OddGroup + (OddGroup > 0 ? 1:0);  // Waiting for a better way to convert boolean into int  
+        
+    }
+}
+/*i & 1 == i%2==1*/
