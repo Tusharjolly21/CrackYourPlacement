e165e080c4dfddc8952e5e267cab1da631031dab
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-07-20 15:03:01 +0530

    Time: 18 ms (9.60%), Space: 40.9 MB (47.18%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int fib(int n) {
-        return dp(n,new int[n+1]);
-    }
-    private int dp(int n,int[] dp){
-        if(n==0 || n==1) return n;
-        if(dp[n]!=0) return dp[n];
-        int fib1 = dp(n-1,dp);
-        int fib2 = dp(n-2,dp);
-        int ans = fib1+fib2;
-        dp[n] = ans;
-        return ans;
+        if(n==0 ){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
     }
 }
