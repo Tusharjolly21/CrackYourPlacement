cc3e20bc7850fd919390e611853f4cbfcda538b8
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-07-21 12:09:27 +0530

    Time: 1 ms (96.99%), Space: 54.4 MB (28.49%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,22 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] dummy1 = new int[matrix.length];
-        int[] dummy2 = new int[matrix[0].length];
-        Arrays.fill(dummy1,-1);
-        Arrays.fill(dummy2,-1);
+        int col0 = 1;
         for(int i = 0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    dummy1[i] = dummy2[j] = 0;
-                }
-            }
+            if(matrix[i][0] ==0) col0 = 0;
+            for(int j = 1;j<matrix[0].length;j++)
+                if(matrix[i][j]==0)
+                    matrix[i][0] = matrix[0][j] = 0;
+                
+            
         }
-        for(int i = 0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[0].length;j++){
-                if(dummy1[i]==0 || dummy2[j]==0){
+        for(int i= matrix.length-1;i>=0;i--){
+            for(int j = matrix[0].length-1;j>=1;j--)
+                if(matrix[i][0]==0 || matrix[0][j]==0)
                     matrix[i][j] = 0;
-                }
-            }
+                if(col0==0) matrix[i][0] = 0;
+            
         }
+        
     }
 }
