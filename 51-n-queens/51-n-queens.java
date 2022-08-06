16806099ee31a343240213821a6c29c6768577a4
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-08-06 13:29:12 +0530

    Time: 5 ms (67.50%), Space: 44.9 MB (73.13%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -8,59 +8,55 @@ class Solution {
             }
         }
         List<List<String>> ans = new ArrayList<>();
-        solve(0, board, ans);
+        solveDfs(0, board, ans);
         return ans;
         
     }
-
-    private void solve(int cols, char[][] board, List<List<String>> ans) {
-        if (cols == board.length) {
-            ans.add(construct(board));
+    private void solveDfs(int col, char[][] board, List < List < String >> res){
+        if(col==board.length){
+            res.add(construct(board));
             return;
         }
-        for (int rows = 0; rows < board.length; rows++) {
-            if (isSafe(board, rows, cols)) {
-                board[rows][cols] = 'Q';
-                solve(cols + 1, board, ans);
-                board[rows][cols] = '.';
-                /*There will be some moment when we do not find the answer therefore while going back whenever we do not find the answer just remove the queen where it was placed when you do not fnd the answer just backtrack from there*/
+        for(int row = 0;row<board.length;row++){
+            if(isSafe(row,col,board)){
+                board[row][col] = 'Q';
+                solveDfs(col+1,board,res);
+                board[row][col] = '.';
             }
         }
+        
     }
-
-    private List<String> construct(char[][] board) {
-        List<String> ans = new ArrayList<>();
+    private List < String > construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
         for (int i = 0; i < board.length; i++) {
             String s = new String(board[i]);
-            ans.add(s);
+            res.add(s);
         }
-        return ans;
+        return res;
     }
 
-    private boolean isSafe(char[][] board, int rows, int cols) {
-        /*upper diagonal*/
-        int dupRow = rows;
-        int dupCol = cols;
-        while (rows >= 0 && cols >= 0) {
-            if (board[rows][cols] == 'Q') return false;
-            rows--;
-            cols--;
+    private boolean isSafe(int row, int col, char[][] board) {
+        int duprow = row;
+        int dupcol = col;
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
         }
-        rows = dupRow; 
-        cols = dupCol;
-        /*column check to Q of left*/
-        while (cols >= 0) {
-            if (board[rows][cols] == 'Q') return false;
-            cols--;
+            row = duprow;
+            col = dupcol;
+            while (col >= 0) {
+                if (board[row][col] == 'Q') return false;
+                col--;
+            }
+
+            row = duprow;
+            col = dupcol;
+            while (col >= 0 && row < board.length) {
+                if (board[row][col] == 'Q') return false;
+                col--;
+                row++;
+            }
+            return true;
         }
-        rows = dupRow; 
-        cols = dupCol; 
-        /*lower left diagonal check to left of Q*/
-        while (rows < board.length && cols >= 0) {
-            if (board[rows][cols] == 'Q') return false;
-            cols--;
-            rows++;
-        }
-        return true;
-    }
 }
