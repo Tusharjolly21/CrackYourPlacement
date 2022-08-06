c733957919acd404513fedf97526d0fb3eb619fa
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-08-06 14:25:53 +0530

    Time: 19 ms (49.39%), Space: 42 MB (32.68%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,10 +1,8 @@
 class Solution {
-
     public void solveSudoku(char[][] board) {
         if (board == null || board.length == 0) return;
         solve(board);
     }
-
     private boolean solve(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -24,11 +22,13 @@ class Solution {
         return true;
     }
 
-    private boolean isValid(char[][] board, int rows, int cols, char c) {
+    public boolean isValid(char[][] board, int row, int col, char c) {
         for (int i = 0; i < 9; i++) {
-            if (board[rows][i] == c) return false;
-            if (board[i][cols] == c) return false;
-            if (board[3 * (rows / 3) + i / 3][3 * (cols / 3) + i % 3] == c) return false;
+            if (board[i][col] == c) return false;
+
+            if (board[row][i] == c) return false;
+
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
         }
         return true;
     }
