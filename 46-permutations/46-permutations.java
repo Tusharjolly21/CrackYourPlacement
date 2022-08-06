7077f90222cbc7952b31cf94222313bfac34df68
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2022-08-06 12:09:26 +0530

    Time: 3 ms (47.13%), Space: 44.7 MB (56.39%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,30 +1,27 @@
 class Solution {
-
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         solve(0,nums,ans);
         return ans;
     }
-
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int i,int j,int[] nums){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-
-    private void solve(int index, int[] nums, List<List<Integer>> ans) {
+    public void solve(int index,int[] nums,List<List<Integer>> ans){
         if(index==nums.length){
-            List<Integer> list = new ArrayList<>();
+            List<Integer> ds = new ArrayList<>();
             for(int i = 0;i<nums.length;i++){
-                list.add(nums[i]);
+                ds.add(nums[i]);
             }
-            ans.add(new ArrayList<>(list));
+            ans.add(new ArrayList<>(ds));
             return;
         }
         for(int i = index;i<nums.length;i++){
-            swap(nums,i,index);
+            swap(i,index,nums);
             solve(index+1,nums,ans);
-            swap(nums,i,index); /*backtracking*/
+            swap(i,index,nums);
         }
     }
 }
